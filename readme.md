6b1c0dfb890ae2e618fa9aedc0f03d58a49f7d17
Fares <faressolibie@gmail.com>
2024-06-24 12:28:54 -0300

    fix: ajustes

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 Integração com serviço Niuco
+
+Passos para usar:
+
+1 - Criar um arquivo .env com base no .env.example
+2 - npm run dev
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,35 @@ import express from 'express'
 import { env } from 'process'
 import { AdaptRoute } from './adapters'
 import { GetUserController } from './controllers'
+import cluster from 'cluster'
+import os from 'os'
 
 dotenv.config()
 
-const app = express()
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length
 
-app.use(express.json())
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
 
-app.get('/health', (_, res) => {
-  return res.status(200).send('Server is healthy')
-})
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} morreu`)
+    // Opcional: criar um novo worker quando um morrer
+    // cluster.fork();
+  })
+} else {
+  const app = express()
 
-app.get('/users', AdaptRoute(new GetUserController()))
+  app.use(express.json())
 
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`)
-})
+  app.get('/health', (_, res) => {
+    return res.status(200).send('Server is healthy')
+  })
+
+  app.get('/users', AdaptRoute(new GetUserController()))
+
+  app.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT}`)
+  })
+}
